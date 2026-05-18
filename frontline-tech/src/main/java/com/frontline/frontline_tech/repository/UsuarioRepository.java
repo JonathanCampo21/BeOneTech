7b201911105bc3265be1b98596817252bd1a5d6b
Jonathan Campos <jonathandevfull21@gmail.com>
2026-05-18 15:16:13 -0300

    Update UsuarioRepository.java

--- frontline-tech/src/main/java/com/frontline/frontline_tech/repository/UsuarioRepository.java
+++ frontline-tech/src/main/java/com/frontline/frontline_tech/repository/UsuarioRepository.java
@@ -9,4 +9,7 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     // MÁGICA: O Spring Boot cria a consulta no banco de dados sozinho só de lermos o nome desse método!
     Usuario findByLoginAndSenha(String login, String senha);
+
+    // ---> NOVO: Necessário para o radar encontrar o usuário apenas pelo login do Token JWT!
+    Usuario findByLogin(String login);
 }
