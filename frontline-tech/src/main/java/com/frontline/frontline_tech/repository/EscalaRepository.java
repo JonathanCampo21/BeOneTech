fde8948101449df78034d09612dcd95f7b01333c
Jonathan Campos <jonathandevfull21@gmail.com>
2026-05-24 13:40:50 -0300

    Update EscalaRepository.java

--- frontline-tech/src/main/java/com/frontline/frontline_tech/repository/EscalaRepository.java
+++ frontline-tech/src/main/java/com/frontline/frontline_tech/repository/EscalaRepository.java
@@ -4,6 +4,11 @@ import com.frontline.frontline_tech.model.Escala;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EscalaRepository extends JpaRepository<Escala, Long> {
+    
+    // MÁGICA: O Spring Boot cria a consulta no banco sozinho pra filtrar pelo departamento!
+    List<Escala> findByDepartamento(String departamento);
 }
